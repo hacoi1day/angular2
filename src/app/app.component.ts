da5349b1de13b5eb791b0aaf5bc1c2052d484c4d
hacoi1day <vuha98.tn@gmail.com>
2020-10-14 16:23:00 +0700

    lession 1 component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lession01';
+  public title = 'Lesson 01 - Component';
 }
