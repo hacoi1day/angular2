113b088f76e40bc1940444e1b5988d57a8e47f66
hacoi1day <vuha98.tn@gmail.com>
2020-10-14 18:13:55 +0700

    property - attribute - class - style binding

--- src/app/components/component-to-view/component-to-view.component.ts
+++ src/app/components/component-to-view/component-to-view.component.ts
@@ -13,6 +13,17 @@ export class ComponentToViewComponent implements OnInit {
   public imgLink: stirng = 'https://kenh14cdn.com/thumb_w/660/2018/7/6/275770773371132401083383332079595817009152n-1530872513330570606652.jpg';
   public link: string = 'https://zing.vn';
   public textLink: string = 'Zingnew';
+  public imgWidth: number = 500;
+
+  public isValid: boolean = true;
+
+  public tbWidth: number = 300;
+  public tbBorder: number = 1;
+
+  public isBorder: boolean = true;
+  public isChecked: number = 0; // 1 : add class ; 0 : remove class
+  public isSpecial: boolean = true;
+  public size: number = 24;
 
   public user: object = {
     name: 'havt',
