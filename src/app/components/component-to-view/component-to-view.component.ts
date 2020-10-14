6880595672a6f14425a84f9d6662e56289312922
hacoi1day <vuha98.tn@gmail.com>
2020-10-14 18:20:59 +0700

    component to view

--- src/app/components/component-to-view/component-to-view.component.ts
+++ src/app/components/component-to-view/component-to-view.component.ts
@@ -10,7 +10,7 @@ export class ComponentToViewComponent implements OnInit {
   public name: string = 'havt';
   public age: number = 22;
   public isMarried: boolean = false;
-  public imgLink: stirng = 'https://kenh14cdn.com/thumb_w/660/2018/7/6/275770773371132401083383332079595817009152n-1530872513330570606652.jpg';
+  public imgLink: string = 'https://kenh14cdn.com/thumb_w/660/2018/7/6/275770773371132401083383332079595817009152n-1530872513330570606652.jpg';
   public link: string = 'https://zing.vn';
   public textLink: string = 'Zingnew';
   public imgWidth: number = 500;
@@ -38,7 +38,7 @@ export class ComponentToViewComponent implements OnInit {
 
   showInfo() {
     return `
-      Name: ${this.user.name}
+      Name: name
     `;
   }
 
