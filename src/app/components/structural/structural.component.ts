e306f85252fa57183be24773717a8fbddda1c876
hacoi1day <vuha98.tn@gmail.com>
2020-10-16 00:27:50 +0700

    structural directive for

--- src/app/components/structural/structural.component.ts
+++ src/app/components/structural/structural.component.ts
@@ -14,6 +14,51 @@ export class StructuralComponent implements OnInit {
 
   public age: number = 0;
 
+  public names: string[] = [
+    'TP. HCM',
+    'Hà Nội',
+  ];
+
+  public dataFromServer: string[] = [
+    'TP. HCM',
+    'Hà Nội',
+    'Đà Nẵng',
+    'Thái Nguyên',
+    'Hải Phòng',
+    'Thanh Hóa'
+  ];
+  public products: any[] = [
+
+  ];
+
+  public productsFromServer: any[] = [
+    {
+      id: 1,
+      name: 'Iphone 6S',
+      price: 2500
+    },
+    {
+      id: 2,
+      name: 'Iphone 6S Plus',
+      price: 3000
+    },
+    {
+      id: 3,
+      name: 'Iphone 7',
+      price: 6500
+    },
+    {
+      id: 4,
+      name: 'Iphone 7 Plus',
+      price: 7000
+    },
+    {
+      id: 5,
+      name: 'Samsung Note 10',
+      price: 9000
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -25,6 +70,15 @@ export class StructuralComponent implements OnInit {
 
   onChange(value): void {
     this.isCheck = value;
+  }
+
+  onLoadData():void {
+    this.products = this.productsFromServer;
+  }
+
+  myTrackByFunction(index, item): number {
+    console.log(item.id);
+    return item.id;
   }
 
 }
