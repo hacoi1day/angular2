c11314a9c7240e031048df0dd6b59ce9ec61cd3b
hacoi1day <vuha98.tn@gmail.com>
2020-10-18 21:48:57 +0700

    pipe 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public content: string = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`;
   public name: string = 'lương đình của';
+  public title: string = 'Học Angular 10';
+  public amount: number = 25.142367;
+  public result: number = 0.8514;
+  public names: string[] = [
+    'abc',
+    'def',
+    'xyz',
+    '123',
+    '456',
+    '789'
+  ];
+  public total: number = 75.8;
+  public today: Date = new Date('12/05/2020');
+  public product: any = {
+    id: 1,
+    name: 'iphone 6s',
+    price: 150,
+    status: true
+  };
+
 }
