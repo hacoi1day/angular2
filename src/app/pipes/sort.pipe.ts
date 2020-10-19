d70d148af8be53268b5331c36df59a75ea7760a0
hacoi1day <vuha98.tn@gmail.com>
2020-10-19 14:48:45 +0700

    done custom pipe

--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {Product} from "../models/product";
+import { orderBy } from 'lodash';
 
 @Pipe({
   name: 'sort'
@@ -8,24 +9,12 @@ export class SortPipe implements PipeTransform {
 
   transform(products: Product[], sortBy: string, sortValue: number): Product[] {
     if (sortBy === 'name') {
-      products.sort((a, b) => {
-        if (a.name > b.name)
-          return sortValue;
-        else if (a.name < b.name)
-          return -sortValue;
-        else
-          return 0;
-      });
+      let value: string = sortValue === 1 ? 'asc' : 'desc';
+      products = orderBy(products, ['name'], [value]);
     }
     if (sortBy === 'price') {
-      products.sort((a, b) => {
-        if (a.price > b.price)
-          return sortValue;
-        else if (a.price < b.price)
-          return -sortValue;
-        else
-          return 0;
-      });
+      let value: string = sortValue === 1 ? 'asc' : 'desc';
+      products = orderBy(products, ['price'], [value]);
     }
     return products;
   }
